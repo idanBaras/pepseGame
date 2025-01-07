bdc6fa283e43e498a8e35787e40024756ea1c065
idanBaras <idanb1706@gmail.com>
2025-01-07 05:48:04 +0200

    gameManager ready with comments
    ready game manager and comments on public funcs

--- Pepse/src/pepse/PepseGameManager.java
+++ Pepse/src/pepse/PepseGameManager.java
@@ -1,10 +1,39 @@
 package pepse;
 import danogl.GameManager;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
 
-public class PepseGameManager {
+/**
+ * the game manager class for the project, run the game
+ */
+public class PepseGameManager extends GameManager{
 
+    /**
+     * init game function, empty call for gameManager
+     * @param imageReader Contains a single method: readImage, which reads an image from disk.
+     *                 See its documentation for help.
+     * @param soundReader Contains a single method: readSound, which reads a wav file from
+     *                    disk. See its documentation for help.
+     * @param inputListener Contains a single method: isKeyPressed, which returns whether
+     *                      a given key is currently pressed by the user or not. See its
+     *                      documentation.
+     * @param windowController Contains an array of helpful, self explanatory methods
+     *                         concerning the window.
+     */
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+        UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+    }
+
+    /**
+     * main function, run the game
+     * @param args getting no args
+     */
     public static void main(String[] args) {
-        new GameManager().run();
+        new PepseGameManager().run();
     }
 
 }
